d02b9eb4b57159b9a69af435c55bea81c56eb6f9
Andrii Mostovyi <mostik.ua@gmail.com>
2016-04-24 02:04:57 +0300

    Added base functional for lax preferences

--- ModuleFuzzyLogic/fuzzyPreferenceRelations.Designer.cs
+++ ModuleFuzzyLogic/fuzzyPreferenceRelations.Designer.cs
@@ -30,13 +30,19 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.numericUpAndDownElementsCount = new System.Windows.Forms.NumericUpDown();
-            this.dataGridViewElements = new System.Windows.Forms.DataGridView();
-            this.Xelement = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPageStrictPreferences = new System.Windows.Forms.TabPage();
+            this.tabPageLaxPreferences = new System.Windows.Forms.TabPage();
             this.dataGridViewMu = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageAttitudeOfIndifference = new System.Windows.Forms.TabPage();
+            this.tabPageQuasiEquivalence = new System.Windows.Forms.TabPage();
+            this.tabPageNonDominated = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpAndDownElementsCount)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewElements)).BeginInit();
+            this.tabPageLaxPreferences.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMu)).BeginInit();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -54,29 +60,7 @@
             this.numericUpAndDownElementsCount.Name = "numericUpAndDownElementsCount";
             this.numericUpAndDownElementsCount.Size = new System.Drawing.Size(120, 20);
             this.numericUpAndDownElementsCount.TabIndex = 1;
-            this.numericUpAndDownElementsCount.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
-            // 
-            // dataGridViewElements
-            // 
-            this.dataGridViewElements.AllowUserToAddRows = false;
-            this.dataGridViewElements.AllowUserToDeleteRows = false;
-            this.dataGridViewElements.AllowUserToResizeColumns = false;
-            this.dataGridViewElements.AllowUserToResizeRows = false;
-            this.dataGridViewElements.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridViewElements.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewElements.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Xelement});
-            this.dataGridViewElements.Location = new System.Drawing.Point(15, 70);
-            this.dataGridViewElements.MultiSelect = false;
-            this.dataGridViewElements.Name = "dataGridViewElements";
-            this.dataGridViewElements.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
-            this.dataGridViewElements.Size = new System.Drawing.Size(120, 283);
-            this.dataGridViewElements.TabIndex = 2;
-            // 
-            // Xelement
-            // 
-            this.Xelement.HeaderText = "Element";
-            this.Xelement.Name = "Xelement";
+            this.numericUpAndDownElementsCount.ValueChanged += new System.EventHandler(this.numericUpAndDownElementsCount_ValueChanged);
             // 
             // label2
             // 
@@ -87,6 +71,37 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Number of elements:";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPageStrictPreferences
+            // 
+            this.tabPageStrictPreferences.Location = new System.Drawing.Point(4, 22);
+            this.tabPageStrictPreferences.Name = "tabPageStrictPreferences";
+            this.tabPageStrictPreferences.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageStrictPreferences.Size = new System.Drawing.Size(403, 309);
+            this.tabPageStrictPreferences.TabIndex = 1;
+            this.tabPageStrictPreferences.Text = "Strict Preferences";
+            this.tabPageStrictPreferences.UseVisualStyleBackColor = true;
+            // 
+            // tabPageLaxPreferences
+            // 
+            this.tabPageLaxPreferences.Controls.Add(this.dataGridViewMu);
+            this.tabPageLaxPreferences.Location = new System.Drawing.Point(4, 22);
+            this.tabPageLaxPreferences.Name = "tabPageLaxPreferences";
+            this.tabPageLaxPreferences.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLaxPreferences.Size = new System.Drawing.Size(472, 309);
+            this.tabPageLaxPreferences.TabIndex = 0;
+            this.tabPageLaxPreferences.Text = "Lax Preferences";
+            this.tabPageLaxPreferences.UseVisualStyleBackColor = true;
+            // 
             // dataGridViewMu
             // 
             this.dataGridViewMu.AllowUserToAddRows = false;
@@ -94,27 +109,69 @@
             this.dataGridViewMu.AllowUserToResizeColumns = false;
             this.dataGridViewMu.AllowUserToResizeRows = false;
             this.dataGridViewMu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewMu.Location = new System.Drawing.Point(141, 70);
+            this.dataGridViewMu.Location = new System.Drawing.Point(6, 9);
             this.dataGridViewMu.Name = "dataGridViewMu";
             this.dataGridViewMu.RowHeadersVisible = false;
-            this.dataGridViewMu.Size = new System.Drawing.Size(430, 254);
+            this.dataGridViewMu.Size = new System.Drawing.Size(460, 297);
             this.dataGridViewMu.TabIndex = 4;
+            this.dataGridViewMu.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewMu_CellValueChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageLaxPreferences);
+            this.tabControl1.Controls.Add(this.tabPageStrictPreferences);
+            this.tabControl1.Controls.Add(this.tabPageAttitudeOfIndifference);
+            this.tabControl1.Controls.Add(this.tabPageQuasiEquivalence);
+            this.tabControl1.Controls.Add(this.tabPageNonDominated);
+            this.tabControl1.Location = new System.Drawing.Point(154, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(480, 335);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPageAttitudeOfIndifference
+            // 
+            this.tabPageAttitudeOfIndifference.Location = new System.Drawing.Point(4, 22);
+            this.tabPageAttitudeOfIndifference.Name = "tabPageAttitudeOfIndifference";
+            this.tabPageAttitudeOfIndifference.Size = new System.Drawing.Size(403, 309);
+            this.tabPageAttitudeOfIndifference.TabIndex = 2;
+            this.tabPageAttitudeOfIndifference.Text = "Attitude of indifference";
+            this.tabPageAttitudeOfIndifference.UseVisualStyleBackColor = true;
+            // 
+            // tabPageQuasiEquivalence
+            // 
+            this.tabPageQuasiEquivalence.Location = new System.Drawing.Point(4, 22);
+            this.tabPageQuasiEquivalence.Name = "tabPageQuasiEquivalence";
+            this.tabPageQuasiEquivalence.Size = new System.Drawing.Size(403, 309);
+            this.tabPageQuasiEquivalence.TabIndex = 3;
+            this.tabPageQuasiEquivalence.Text = "Quasi Equivalence";
+            this.tabPageQuasiEquivalence.UseVisualStyleBackColor = true;
+            // 
+            // tabPageNonDominated
+            // 
+            this.tabPageNonDominated.Location = new System.Drawing.Point(4, 22);
+            this.tabPageNonDominated.Name = "tabPageNonDominated";
+            this.tabPageNonDominated.Size = new System.Drawing.Size(403, 309);
+            this.tabPageNonDominated.TabIndex = 4;
+            this.tabPageNonDominated.Text = "tabPage1";
+            this.tabPageNonDominated.UseVisualStyleBackColor = true;
             // 
             // fuzzyPreferenceRelations
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(646, 359);
-            this.Controls.Add(this.dataGridViewMu);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.dataGridViewElements);
             this.Controls.Add(this.numericUpAndDownElementsCount);
             this.Controls.Add(this.label1);
             this.Name = "fuzzyPreferenceRelations";
             this.Text = "fuzzyPreferenceRelations";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpAndDownElementsCount)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewElements)).EndInit();
+            this.tabPageLaxPreferences.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMu)).EndInit();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,9 +181,14 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown numericUpAndDownElementsCount;
-        private System.Windows.Forms.DataGridView dataGridViewElements;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Xelement;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPageStrictPreferences;
+        private System.Windows.Forms.TabPage tabPageLaxPreferences;
         private System.Windows.Forms.DataGridView dataGridViewMu;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageAttitudeOfIndifference;
+        private System.Windows.Forms.TabPage tabPageQuasiEquivalence;
+        private System.Windows.Forms.TabPage tabPageNonDominated;
     }
 }
--- ModuleFuzzyLogic/fuzzyPreferenceRelations.cs
+++ ModuleFuzzyLogic/fuzzyPreferenceRelations.cs
@@ -16,25 +16,28 @@ namespace ModuleFuzzyLogic
         {
             InitializeComponent();
 
-            dataGridViewElements.Rows.Clear();
+           
 
             
         }
         private int elementsCount;
+        private bool blockValidating = false;
+
+
 
         private void changeMatrixSize(int n)
-        {n = 
-            n + 1;
-            //dataGridViewMu.Columns.
-            while (dataGridViewMu.Columns.Count < n)
+        {
+            blockValidating = true;
+            int oldN = dataGridViewMu.Columns.Count-1;
+            while (dataGridViewMu.Columns.Count < n+1)
             {
                 string nm = "X" + dataGridViewMu.Columns.Count;
                 if (dataGridViewMu.Columns.Count == 0)
                     nm = "";
                 dataGridViewMu.Columns.Add(nm,nm);
-                dataGridViewMu.Columns[dataGridViewMu.Columns.Count - 1].Width = 30;
+                dataGridViewMu.Columns[dataGridViewMu.Columns.Count - 1].Width = 35;
             }
-            while (dataGridViewMu.Columns.Count > n)
+            while (dataGridViewMu.Columns.Count > n+1)
             {
                 dataGridViewMu.Columns.RemoveAt(dataGridViewMu.Columns.Count - 1);
             }
@@ -48,31 +51,90 @@ namespace ModuleFuzzyLogic
                 string nm = "X" + dataGridViewMu.Columns.Count;
                 if (dataGridViewMu.Columns.Count == 0)
                     nm = "";
+                
                 dataGridViewMu.Rows.Add();
                 dataGridViewMu[0, dataGridViewMu.RowCount - 1].Value = "X" + dataGridViewMu.RowCount;
+            }
+            Random generator = new Random();
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (i < oldN && j < oldN) continue;
+                    if (i == j)
+                    {
+                        dataGridViewMu[i + 1, j].Value = 1;
+                    }
+                    else
+                    {
+                        dataGridViewMu[i + 1, j].Value = generator.Next(0, 100) / 100.0;
+                    }
+                }
             }
 
             for (int i = 0; i < n; ++i)
             {
-                dataGridViewMu[0, i].ReadOnly = true;
-                dataGridViewMu[i, 0].ReadOnly = true;
+                for (int j = 1; j <= n; ++j)
+                {
+                    dataGridViewMu[j, i].ReadOnly = false;
+                }
             }
+
+
+            for (int i = 0; i < n; ++i)
+            {
+                dataGridViewMu[0, i].ReadOnly = true;
+                dataGridViewMu[i+1, i].ReadOnly = true;
+            }
+            blockValidating = false;
         }
 
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        private void numericUpAndDownElementsCount_ValueChanged(object sender, EventArgs e)
         {
             elementsCount = decimal.ToInt32(numericUpAndDownElementsCount.Value);
             changeMatrixSize(elementsCount);
-            while (dataGridViewElements.RowCount < elementsCount)
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+            for (int i = 0; i < elementsCount; i++)
             {
-                dataGridViewElements.Rows.Add();
-                dataGridViewElements[0, dataGridViewElements.RowCount - 1].Value = 1;
+                for (int j = 0; j < elementsCount; j++)
+                {
+                    if (i == j)
+                    {
+                        dataGridViewMu[i + 1, j].Value = 1;
+                    }
+                    else
+                    {
+                        dataGridViewMu[i + 1, j].Value = generator.Next(0, 100) / 100.0;
+                    }
+                }
             }
+        }
 
-            while (dataGridViewElements.RowCount > elementsCount)
-                dataGridViewElements.Rows.RemoveAt(dataGridViewElements.RowCount - 1);
-
-            //changeExpertsCount();
+        private void dataGridViewMu_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (blockValidating) return;
+            string w = dataGridViewMu[e.ColumnIndex, e.RowIndex].Value.ToString();
+            double q;
+            if (!Double.TryParse(w, out q))
+            {
+                throw new Exception("BAD ARGIMENT");
+            }
+            if (q<0 || q>1)
+            {
+                throw new Exception("BAD ARGIMENT");
+            }
+            string str = dataGridViewMu[e.ColumnIndex, e.RowIndex].Value.ToString();
+            if (str.Length > 5)
+            {
+                dataGridViewMu[e.ColumnIndex, e.RowIndex].Value = str.Substring(0, 5);
+            }
+            
         }
     }
 }
