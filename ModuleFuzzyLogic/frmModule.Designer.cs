24cf1ba3d620e07b8847e536406d7f1ed0313059
Lifni <lifni.klz@gmail.com>
2016-04-25 14:41:31 +0300

    fix main form

--- ModuleFuzzyLogic/frmModule.Designer.cs
+++ ModuleFuzzyLogic/frmModule.Designer.cs
@@ -34,14 +34,17 @@
             this.buttonOperOnRelation = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(9, 10);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(2, 162);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 41);
+            this.button1.Size = new System.Drawing.Size(459, 37);
             this.button1.TabIndex = 0;
             this.button1.Text = "Andrii";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,9 +52,10 @@
             // 
             // binRelationsPropButton
             // 
-            this.binRelationsPropButton.Location = new System.Drawing.Point(137, 12);
+            this.binRelationsPropButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.binRelationsPropButton.Location = new System.Drawing.Point(3, 123);
             this.binRelationsPropButton.Name = "binRelationsPropButton";
-            this.binRelationsPropButton.Size = new System.Drawing.Size(119, 51);
+            this.binRelationsPropButton.Size = new System.Drawing.Size(457, 34);
             this.binRelationsPropButton.TabIndex = 0;
             this.binRelationsPropButton.Text = "Властивості бін. відн.";
             this.binRelationsPropButton.UseVisualStyleBackColor = true;
@@ -59,10 +63,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(196, 10);
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(2, 2);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(89, 41);
+            this.button3.Size = new System.Drawing.Size(459, 36);
             this.button3.TabIndex = 0;
             this.button3.Text = "Dmytro";
             this.button3.UseVisualStyleBackColor = true;
@@ -70,22 +75,23 @@
             // 
             // buttonOperOnRelation
             // 
-            this.buttonOperOnRelation.Location = new System.Drawing.Point(9, 68);
+            this.buttonOperOnRelation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonOperOnRelation.Location = new System.Drawing.Point(2, 42);
             this.buttonOperOnRelation.Margin = new System.Windows.Forms.Padding(2);
             this.buttonOperOnRelation.Name = "buttonOperOnRelation";
-            this.buttonOperOnRelation.Size = new System.Drawing.Size(124, 41);
+            this.buttonOperOnRelation.Size = new System.Drawing.Size(459, 36);
             this.buttonOperOnRelation.TabIndex = 0;
             this.buttonOperOnRelation.Text = "Операції над нечіткими відношенннями";
             this.buttonOperOnRelation.UseVisualStyleBackColor = true;
             this.buttonOperOnRelation.Click += new System.EventHandler(this.buttonOperOnRelation_Click);
-
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(152, 68);
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Location = new System.Drawing.Point(2, 82);
             this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(134, 41);
+            this.button6.Size = new System.Drawing.Size(459, 36);
             this.button6.TabIndex = 0;
             this.button6.Text = "Sofiia";
             this.button6.UseVisualStyleBackColor = true;
@@ -100,20 +106,38 @@
             this.button7.Text = "button1";
             this.button7.UseVisualStyleBackColor = true;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.button6, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.binRelationsPropButton, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonOperOnRelation, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(463, 201);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
             // frmModule
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(302, 129);
+            this.ClientSize = new System.Drawing.Size(463, 201);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.buttonOperOnRelation);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.binRelationsPropButton);
-            this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmModule";
             this.Text = "frmModule";
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -126,5 +150,6 @@
         private System.Windows.Forms.Button buttonOperOnRelation;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
